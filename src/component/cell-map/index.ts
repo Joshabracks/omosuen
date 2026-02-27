d5404b106f93d6a2601b1eece5e8503bea1c8f19
Joshua Bracks <joshabracks@gmail.com>
2026-02-27 12:50:20 -0800

    global cell-map

--- src/component/cell-map/index.ts
+++ src/component/cell-map/index.ts
@@ -3,6 +3,24 @@ export {
   PROPERTY_ALLOWLIST,
   generateDefaultCubeMesh,
   CellMapSerializer,
+  resetCellMapState,
+  // Module-level singleton data (prefixed to avoid naming conflicts)
+  cmMaterials,
+  cmMaterialMap,
+  cmShapeMap,
+  cmMeshes,
+  cmEmissionMap,
+  cmVisibilityMap,
+  cmCellSize,
+  cmMapSize,
+  cmPackedData,
+  cmSmoothing,
+  cmSmoothingWeights,
+  cmNormalSmoothing,
+  cmNeedsGPUUpdate,
+  cmChunks,
+  cmChunkGridSize,
+  cmRevealExempt,
 } from './data';
 export type { CellMapT, CellMapOptions } from './data';
 export { CellMap } from './methods';
